f5cd253133bd00ea6e59a8ef50961b4035331d92
Jun4928 <jun.choi.4928@gmail.com>
2021-07-29 09:53:10 +0900

    Modify: entries sum

--- group.js
+++ group.js
@@ -4,6 +4,7 @@ const {
   noop,
   groupBy,
   go,
+  omit,
   each,
   entries,
   map,
@@ -17,36 +18,46 @@ const {
   zipObj,
   union,
   flat,
+  indexBy,
+  values,
+  object,
 } = require('fxjs');
 const L = require('fxjs/Lazy');
 
 const stats = [
   {
+    property_id: 1,
     keyword_name: 'a',
     cnt: 11,
     price: 10,
   },
   {
+    property_id: 2,
     keyword_name: 'c',
     cnt: 22,
     price: 20,
   },
   {
+    property_id: 3,
     keyword_name: 'b',
     cnt: 33,
     price: 30,
   },
+
   {
+    property_id: 1,
     keyword_name: 'a',
     cnt: 44,
     price: 10,
   },
   {
+    property_id: 2,
     keyword_name: 'b',
     cnt: 55,
     price: 30,
   },
   {
+    property_id: 3,
     keyword_name: 'c',
     cnt: 66,
     price: 100,
@@ -66,12 +77,31 @@ const sumOfNumberProperties = reduce(
 
 const group = go(
   stats,
-  groupBy(({ keyword_name }) => keyword_name),
+  groupBy(({ property_id }) => property_id),
   L.values,
-  L.map(sumOfNumberProperties),
-  sortByDesc(['cnt', 'price']),
+  L.map(L.map((stat) => omit(['property_id'], stat))),
+  map(sumOfNumberProperties),
 );
 
-const nonAdStats = [1, 2, 3];
+console.log(group);
 
-console.log(go([group, nonAdStats], flat));
+const group2 = go(
+  stats,
+  groupBy(({ property_id }) => property_id),
+  L.entries,
+  L.map(([p_id, stats]) => [
+    p_id,
+    go(
+      stats,
+      L.map((stat) => omit(['property_id'], stat)),
+      sumOfNumberProperties,
+    ),
+  ]),
+  object,
+);
+
+console.log(group2);
+
+// const nonAdStats = [1, 2, 3];
+
+// console.log(go([group, nonAdStats], flat));
